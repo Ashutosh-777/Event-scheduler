6579a12695375ff5c995725a30928e2b2f511ca8
Ashutosh-777 <ashutosh.ghosty@gmail.com>
2022-12-20 13:38:54 +0530

    first commit

---
+++ lib/homepage.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'next_page.dart';
+
+class MyApp extends StatefulWidget {
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final List<String> _eventlist=[] ;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.deepPurpleAccent,
+        appBar: AppBar(
+          title: const Center(child: Text('Event Scheduler',
+            style: TextStyle(fontSize: 23,color: Colors.white),
+
+          )),
+          backgroundColor: Colors.deepPurpleAccent[700],
+
+        ),
+        body:
+        Center(
+          child: ListView.builder(
+
+            padding: const EdgeInsets.symmetric(vertical: 8),
+            itemCount: _eventlist.length,
+            itemBuilder: (context,index) {
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ListTile(
+                  tileColor: Colors.white,
+                  title: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(_eventlist[index],
+
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+
+                ),
+              );
+
+
+            },
+          ),
+        ),
+        floatingActionButton: Builder(
+            builder: (context) {
+              return FloatingActionButton(
+                onPressed: () async{
+                  String input_event =await Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => next_page()));
+                  setState(() {
+                    _eventlist.add(input_event);
+                  });
+                },
+                child: const Icon(Icons.add),
+              );
+            }
+        ),
+      ),
+    );
+  }
+}
+
+
+
---
+++ lib/next_page.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'homepage.dart';
+
+
+class next_page extends StatefulWidget {
+  const next_page({Key? key}) : super(key: key);
+
+  @override
+  State<next_page> createState() => _next_pageState();
+}
+
+class _next_pageState extends State<next_page> {
+  final TextEditingController textfield_input_controller =TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return
+      Scaffold(
+        appBar: AppBar(
+          title: const Text('New Event'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: TextField(
+
+                  enabled: true,
+                  onTap: () {
+
+                  },
+
+                  cursorColor: Colors.deepPurpleAccent[700],
+                  controller: textfield_input_controller,
+                  decoration: InputDecoration(
+                    enabled: true,
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(22),
+                      borderSide: const BorderSide(
+
+                        color: Colors.deepPurpleAccent
+                      ),
+                    ),
+                    focusedBorder:OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(22),
+                      borderSide: const BorderSide(
+
+                          color: Colors.deepPurpleAccent
+                      ),
+                    ),
+                    filled: true,
+                    fillColor: Colors.deepPurpleAccent,
+
+                    prefixIcon: const Icon(
+                      Icons.calendar_month,
+                      color: Colors.white,
+                    ),
+                    hintText: 'Event',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(22),
+                    ),
+                  ),
+                  style: const TextStyle(
+
+                    fontSize: 24,
+                  ),
+                  minLines: 1,
+                  maxLines: 8,
+                ),
+              ),
+
+        SizedBox(
+                    width: 420,
+                    height: 60,
+                    child: Builder(
+                      builder: (context) {
+                        return ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(22),
+                            ),
+                            backgroundColor: Colors.grey,
+                          ),
+                          onPressed: () {
+                            String Textfield_input = textfield_input_controller.text;
+
+                            Navigator.of(context).pop(Textfield_input);
+                            debugPrint(Textfield_input);
+
+                          },
+                          child: const Text(
+                            'ADD',
+                            style: TextStyle(
+                              fontSize: 18,
+                            ),
+                          ),
+                        );
+                      }
+                    ),
+                  )
+
+
+            ],
+          ),
+        ),
+      );
+
+  }
+}
